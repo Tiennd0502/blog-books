89f87a932aff23d7dfba48dc6fcbe6389469ee62
tiennguyen <tiennd0502@gmail.com>
2022-11-23 17:16:17 +0700

    feat(component): update logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -20,7 +20,7 @@ const Logo: React.FC = () => {
           blurDataURL={`/_next/image?url=${IMAGES.LOGO_URL}&w=16&q=1`}
           loader={imageLoader}
           priority
-          sizes="50vw"
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 20vw"
         />
       </Box>
     </Link>
